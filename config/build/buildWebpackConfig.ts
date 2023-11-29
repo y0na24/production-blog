4e069c4048da04ccb1d76c4b95a80974579be10f
Матвей Клёнов <y0na24@mail.ru>
2023-11-29 19:18:31 +0300

    add dev server for webpack

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import { BuildOptions } from './types/config'
 import { buildLoaders } from './buildLoaders'
 import { buildResolvers } from './buildResolvers'
 import { buildPlugins } from './buildPlugins'
+import { buildDevServer } from './buildDevServer'
 
 export function buildWebpackConfig(options: BuildOptions): Configuration {
-  const { paths, mode } = options
+  const { paths, mode, isDev } = options
 
   return {
     mode,
@@ -22,5 +23,7 @@ export function buildWebpackConfig(options: BuildOptions): Configuration {
     resolve: buildResolvers(),
 
     plugins: buildPlugins(options),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   }
 }
