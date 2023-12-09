d2f4e2166a864a2b8bcd3c8ccea99e7866cd8237
y0na24 <y0na24@mail.ru>
2023-12-09 10:39:17 +0300

    add pipeline

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -9,7 +9,7 @@ export function buildPlugins({
   paths,
   isDev
 }: BuildOptions): WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html
     }),
@@ -20,10 +20,17 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    }),
-    new ReactRefreshWebpackPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ]
+
+  if (isDev) {
+    plugins.push(
+      new ReactRefreshWebpackPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      })
+    )
+  }
+
+  return plugins
 }
