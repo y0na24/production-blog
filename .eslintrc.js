fbfa97665a4a250f6718f08bf611f156e82eec9d
y0na24 <y0na24@mail.ru>
2023-12-13 19:42:36 +0300

    add json-serer

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     '@typescript-eslint/no-confusing-void-expression': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
-    '@typescript-eslint/consistent-type-imports': 'off'
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   }
 }
