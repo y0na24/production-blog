4c0d7daf2e580a94e76f3fe74697f9db50ea4042
y0na24 <y0na24@mail.ru>
2023-12-17 16:10:41 +0300

    replace bundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -22,14 +22,14 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev)
     })
   ]
-  plugins.push(
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
-    })
-  )
 
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin())
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      })
+    )
   }
 
   return plugins
